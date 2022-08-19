d80d295770da5df7b8082f7160779e96f982c111
rumjuice <ramdhani@live.com>
2022-08-19 15:51:27 -0400

    adjust styling

--- apps/dashboard/src/components/Header.component.tsx
+++ apps/dashboard/src/components/Header.component.tsx
@@ -4,9 +4,13 @@ import { Profile, SignOut } from ".";
 const Header = (): ReactElement => {
   return (
     <header className="sticky z-10 top-0 w-full py-2 px-4 bg-primary-dark">
-      <div className="w-2/3 m-auto flex justify-between text-white">
+      <div className="w-2/3 sm:w-4/5 m-auto flex justify-between text-white">
         <div>
-          <img src="logo.png" alt="Land River" className="w-100 h-8" />
+          <img
+            src="logo-full-white.png"
+            alt="Land River"
+            className="w-full h-8"
+          />
         </div>
         <div className="flex gap-4 items-center">
           <span className="font-medium">Admin</span>
